acad04a5047f1950cbddb005971c9017dc5f7eb4
papa-react <makiya112545@gmail.com>
2021-10-03 21:34:45 -1200

    add firebase api key

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -3,9 +3,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    
+    apiKey: "AIzaSyCgSlaWlwvpdlczOJ39TQxBTLCdd7KNQZM",
+    authDomain: "gamil-clone-with-nathan.firebaseapp.com",
+    projectId: "gamil-clone-with-nathan",
+    storageBucket: "gamil-clone-with-nathan.appspot.com",
+    messagingSenderId: "560695019982",
+    appId: "1:560695019982:web:cc47eed71040d6f3cec39f"
 };
-
 
 firebase.initializeApp(firebaseConfig);
 
